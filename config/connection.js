8566fbc612ae0dcfb57b961448f70daedb278919
StarfoxMulder <cjprestia@gmail.com>
2017-01-24 14:59:22 -0500

    Not sure what happened to the previous version I had of this portfolio that I built over thanksgiving, but it might just be lost from switching computers around that time... spent last few hours just getting this near presentatble

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,21 @@
 
-var mysql = require('mysql');
-var key = require('./privateConnection.js')
+// var mysql = require('mysql');
+// var key = require('./privateConnection.js')
 
-var connection;
+// var connection;
 
-if (process.env.JAWSDB_URL) {
-	connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-    connection = key.privateConnection;
-};
+// if (process.env.JAWSDB_URL) {
+// 	connection = mysql.createConnection(process.env.JAWSDB_URL);
+// } else {
+//     connection = key.privateConnection;
+// };
 
-connection.connect(function(err) {
-    if (err) {
-        console.error('error connecting: ' + err.stack);
-        return;
-    }
-    console.log('connected as id ' + connection.threadId);
-});
+// connection.connect(function(err) {
+//     if (err) {
+//         console.error('error connecting: ' + err.stack);
+//         return;
+//     }
+//     console.log('connected as id ' + connection.threadId);
+// });
 
-module.exports = connection;
+// module.exports = connection;
--- server.js
+++ server.js
@@ -1,79 +1,62 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
+var path = require('path');
+var logger = require("morgan");
+var mongoose = require("mongoose");
+var Router = express.Router();
+var routes = require("./controllers/controller.js");
+var request = require("request");
+var Promise = require("bluebird");
 
+mongoose.Promise = Promise;
 
-// bring in the models
-var models = require('./models');
-
-// sync the models
-models.sequelize.sync({});
-
-// Instantiate Express
+// Initialize Express
 var app = express();
-//Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static(__dirname + '/public'));
 
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
-// override with POST having ?_method=DELETE
+app.use(logger("dev"));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
+app.use(express.static("./public"));
+app.use(routes);
+
 var exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs({
    defaultLayout: 'main'
 }));
 app.set('view engine', 'handlebars');
 
+// Configuring protected_dust database
+var databaseUri = "mongodb://localhost/portfolio";
+if (process.env.MONGODB_URI) {
+  mongoose.connect(process.env.MONGODB_URI);
+} else {
+  mongoose.connect(databaseUri);
+}
+var db = mongoose.connection;
+
+// Show any mongoose errors
+db.on("error", function(error) {
+  console.log("Mongoose Error: ", error);
+});
+
+// Once logged in to the db through mongoose, log a success message
+db.once("open", function() {
+  console.log("Mongoose connection successful.");
+});
+
+
+var port = process.env.PORT || 3030;
+app.listen(port, function() {
+  console.log("App running on port "+ port);
+});
+
 // bring in the routes
 var routes = require('./controllers/controller.js');
 
 // connect the routes
 app.use('/', routes);
-app.use('/update', routes);
-app.use('/create', routes);
 
-
-// listen on port 3000
-var port = process.env.PORT || 3000;
-app.listen(port);
 
 console.log(module.exports);
 
-//// It was hard for me to reconcile this offhand, but went with Mary's version and elected to save the previous version of our server.js until we're certain that we want our file to look this way
-//
-// <<<<<<< HEAD
-// var express = require('express');
-// var bodyParser = require('body-parser');
-// var models = require('./models');
-// var methodOverride = require('method-override');
-// var path = require('path');
-// var exphbs = require('express-handlebars');
-// var router = require('./controllers/ML_controller.js');
-// var multer = require('multer');
-
-// var app = express();
-// var upload = multer();
-
-// models.sequelize.sync({});
-// // Serve static content for the app from the "public" directory in the application directory.
-// app.use(express.static(process.cwd() + '/public'));
-
-// // parse application/x-www-form-urlencoded
-// app.use(bodyParser.urlencoded({extended: true}));
-
-// // override with POST having ?_method=DELETE
-// app.use(methodOverride('_method'));
-// // handlebars setup
-// app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-// app.set('view engine', 'handlebars');
-
-// app.use('/', router);
-
-
-// var port = process.env.PORT || 3000;
-// app.listen(port, function () {
-//   console.log('Listening on PORT ' + port);
-// });
-// =======
